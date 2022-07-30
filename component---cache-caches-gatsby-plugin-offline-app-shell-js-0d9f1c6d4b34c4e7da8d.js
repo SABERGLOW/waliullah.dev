dcf392f10d18f60d2df15a6d3382187945491797
SABERGLOW <wali6985@gmail.com>
2022-07-30 21:47:14 +0200

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-0d9f1c6d4b34c4e7da8d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdevfolio=self.webpackChunkdevfolio||[]).push([[306],{1930:function(e,t,n){var u=n(4836);t.__esModule=!0,t.default=void 0;var l=u(n(7867)),f=u(n(7294)),o=function(e){function t(){return e.apply(this,arguments)||this}return(0,l.default)(t,e),t.prototype.render=function(){return f.default.createElement(f.default.Fragment,null)},t}(f.default.Component),r=o;t.default=r}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-0d9f1c6d4b34c4e7da8d.js.map
